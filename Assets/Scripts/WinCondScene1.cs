243b87739bcab50db4f066d5a92fbd99eef08786
Pasquier Baptiste <baptiste.pasquier8@gmail.com>
2021-11-26 10:57:42 +0100

    minor changes

--- Assets/Scripts/WinCondScene1.cs
+++ Assets/Scripts/WinCondScene1.cs
@@ -17,46 +17,55 @@ public class WinCondScene1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        winpanel =  GameObject.FindGameObjectWithTag("winscene1");
+        winpanel = GameObject.FindGameObjectWithTag("winscene1");
+        winpanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        int a = 0,b = 0; // init de compteurs
+        int a = 0, b = 0; // init de compteurs
 
         //recup des dropzones
         tmp = GetComponentsInChildren<dropscript>();
+        print("sale pute");
         // separation en fonction de leur etat dans les tableau tags et tagsR
-        foreach(dropscript k in tmp)
+        foreach (dropscript k in tmp)
         {
-            if(k.right == true)
+            print("bougnoule");
+            if (k.right == true)
             {
                 tags[a] = k.filed;
                 a++;
             }
 
-            if(k.right == false)
+            if (k.right == false)
             {
                 tagsR[b] = k.filed;
+                print("aled");
                 b++;
             }
 
         }
-        // étude des dropzones
-        foreach(bool j in tags)
+        print("negre");
+        // Ã©tude des dropzones
+        foreach (bool j in tags)
         {
             if (j == true)
-                count_filled++;
+                print("jeanne");
+            count_filled++;
         }
+        print("puuuute");
         foreach (bool j in tagsR)
         {
             if (j == true)
-                count_n_filled++;
+                print("oskour");
+            count_n_filled++;
         }
+        print("a l'air de marcher");
         //condition de victoire
         if (count_filled == length && count_n_filled == 0)
             winpanel.SetActive(true);
-            
+
     }
 }
